4ad8c4c38a51d2b318ad16c24ec250625960431b
Manuel Mannhardt <manuel.mannhardt95@gmail.com>
2023-03-29 23:42:49 +0200

    added feater which allows the user to view a chart displaying the average of each session. also added gitignore to data/charts directory

--- Climbing/Bot/Bot.py
+++ Climbing/Bot/Bot.py
@@ -4,7 +4,10 @@ from Climbing.Util.Helper import chunks, create_pretty_table
 from Climbing.Util.Telegram import Telegram
 
 from collections import OrderedDict
+import datetime
 from datetime import date
+import matplotlib
+from matplotlib import pyplot
 from string import digits
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler, Filters
@@ -34,6 +37,7 @@ class Bot:
         self._telegram.add_command(CommandHandler('start', self._start))
         self._telegram.add_command(CommandHandler('end', self._end))
         self._telegram.add_command(CommandHandler('help', self._help))
+        self._telegram.add_command(CommandHandler('chart', self._chart, pass_args=True))
         self._telegram.add_command(CommandHandler('ranking', self._ranking, pass_args=True))
         self._telegram.add_command(CommandHandler('session', self._session, pass_args=True))
         self._telegram.add_command(CommandHandler('stats', self._stats, pass_args=True))
@@ -64,6 +68,62 @@ class Bot:
             :return None
         """
         update.message.reply_text(reply_markup=ReplyKeyboardRemove())
+
+    def _chart(self, update: Update, context: CallbackContext) -> None:
+        """
+            Generates a chart and sends it to the user
+
+            :param update: Update
+                The context of the current message
+            :param context: CallbackContext
+                The callback context
+            :return None
+        """
+        username = context.args[0] if context.args else update.message.from_user.first_name
+        routes = self._database.get_routes(username=username)
+        routes_grouped_by_date = OrderedDict()
+        for route in routes:
+            date_ = route[3]
+            if date_ not in routes_grouped_by_date.keys():
+                routes_grouped_by_date[date_] = []
+
+            routes_grouped_by_date[date_].append(route)
+
+        # Calculate the average grade for each date for both bouldering and climbing
+        averages_bouldering = OrderedDict()
+        averages_climbing = OrderedDict()
+        grades = Grade.get_grades()
+        for date_, routes in routes_grouped_by_date.items():
+            bouldering_grades = [grades[route[2]] for route in routes if route[2].startswith('V')]
+            climbing_grades = [grades[route[2]] for route in routes if not route[2].startswith('V')]
+
+            averages_bouldering[date_] = sum(bouldering_grades) / len(bouldering_grades) if bouldering_grades else 0
+            averages_climbing[date_] = sum(climbing_grades) / len(climbing_grades) if climbing_grades else 0
+
+        del routes_grouped_by_date
+
+        # Sort the data by ascending date
+        bouldering_chart_data = OrderedDict(sorted(averages_bouldering.items(), key=lambda x: datetime.datetime.strptime(x[0], '%Y-%m-%d')))
+        climbing_chart_data = OrderedDict(sorted(averages_climbing.items(), key=lambda x: datetime.datetime.strptime(x[0], '%Y-%m-%d')))
+        del averages_bouldering
+        del averages_climbing
+
+        # Draw the chart and save it to charts/USERNAME.png
+        # TODO remove hardcoded path
+        matplotlib.use('agg')
+        pyplot.title('Average grade for bouldering/climbing in each session.')
+        pyplot.xlabel('Date')
+        pyplot.ylabel('Grade')
+        pyplot.plot(bouldering_chart_data.keys(), bouldering_chart_data.values(), label='Bouldering')
+        pyplot.plot(climbing_chart_data.keys(), climbing_chart_data.values(), label='Climbing')
+        pyplot.legend()
+        file_path = f'charts/{username}.png'
+        pyplot.savefig(file_path)
+        pyplot.clf()
+
+        # Send the image back to the user
+        with open(file_path, 'rb') as file_:
+            update.message.reply_photo(file_)
 
     def _ranking(self, update: Update, context: CallbackContext) -> None:
         """
@@ -196,7 +256,9 @@ class Bot:
             '/start':              'Starts a session and displays the grade keyboard.',
             '/end':                'Ends a session and removes the grade keyboard again.',
             '/ranking':            'Displays a ranking for all users of this bot.',
-            '/session':            'Diplays your statistics of the current session.',
+            '/chart':              'Displays a chart with data of the topped routes.',
+            '/chart [username]':   'Displays a chart with data of the topped routes for the given user.',
+            '/session':            'Displays your statistics of the current session.',
             '/session [username]': 'Displays the statistics of the given user of the current session.',
             '/stats':              'Displays your all time statistics',
             '/stats [username]':   'Displays the all time statistics of the given user.',
